61524964077f5acea70840f3b48bede00619544f
Leigh Halliday <leighhalliday@gmail.com>
2020-10-29 07:44:14 -0400

    Completed demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,58 @@ import { useRef, useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [image, setImage] = useState<File>();
+  const [preview, setPreview] = useState<string>();
+  const fileInputRef = useRef<HTMLInputElement>();
+
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(image);
+    } else {
+      setPreview(null);
+    }
+  }, [image]);
+
   return (
     <div className={styles.container}>
-      <form>form</form>
+      <form>
+        {preview ? (
+          <img
+            src={preview}
+            style={{ objectFit: "cover" }}
+            onClick={() => {
+              setImage(null);
+            }}
+          />
+        ) : (
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              fileInputRef.current.click();
+            }}
+          >
+            Add Image
+          </button>
+        )}
+        <input
+          type="file"
+          style={{ display: "none" }}
+          ref={fileInputRef}
+          accept="image/*"
+          onChange={(event) => {
+            const file = event.target.files[0];
+            if (file && file.type.substr(0, 5) === "image") {
+              setImage(file);
+            } else {
+              setImage(null);
+            }
+          }}
+        />
+      </form>
     </div>
   );
 }
