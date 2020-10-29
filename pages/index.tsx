061b3c3e14d490456028c87cc50b65dee22b7e2d
Leigh Halliday <leighhalliday@gmail.com>
2020-10-29 07:20:09 -0400

    Starting point

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { useRef, useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <form>form</form>
+    </div>
+  );
+}
